bbb23cc921a896c17b761c5f098a5a0931d9e6fe
Basil <Vaseokk@gmail.com>
2020-10-08 21:46:39 +0300

    added the footer section + css :))

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,37 @@
 import React from 'react'
 
+import fb_logo from '../assets/facebook.png';
+import ig_logo from '../assets/instagram.png';
+
 import './Footer.css';
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      Footer
+    <div className="footer">
+      <div className="footer-block-one">
+        <div className="footer-next">Next Steps</div>
+        <div className="footer-text">
+          Cras pulvinar mattis nunc sed. Viverra nam libero justo laoreet sit
+          amet cursus sit amet. Dolor sit amet consectetur
+        </div>
+        <div className="footer-img">
+          <img className="footer-fb" src={fb_logo} alt="" />
+          <img className="footer-ig" src={ig_logo} alt="" />
+        </div>
+      </div>
+        <div className='footer-line'/>
+      <div className="footer-block-two">
+        <div className="footer-menu-align-first">
+          <div className="footer-menu">Portfolio</div>
+          <div className="footer-menu">Blog</div>
+        </div>
+        <div className="footer-menu-align-second">
+          <div className="footer-menu">Pricing</div>
+          <div className="footer-menu">Contact us</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer;
